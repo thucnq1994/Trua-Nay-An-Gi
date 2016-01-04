4fee8af37fbfaee6cbefc2878604f2a554578000
Nguyen Quang Thuc <thucnq1994@gmail.com>
2016-01-04 12:26:58 +0700

    change process.env settings

--- config/production.config.js
+++ config/production.config.js
@@ -3,12 +3,12 @@
 module.exports = function (ROOT_PATH) {
   var config = {
     server: {
-      port: 5000,
+      port: process.env.PORT,
       hostname: 'trua-nay-an-gi.herokuapp.com',
     },
     database: {
-      //url: 'mongodb://localhost/trua_nay_an_gi'
-      url: 'mongodb://thucnq1994:123456987@ds031611.mongolab.com:31611/heroku_3d6nn3wv'
+      url: process.env.MONGOLAB_URI || 'mongodb://localhost/trua_nay_an_gi'
+
     },
     google: {
       CLIENT_ID : '306070905265-l15ufott6ebb2vqhij10ffrcq3r1mlak.apps.googleusercontent.com',
